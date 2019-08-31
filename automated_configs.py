9c9146358f1d0bb2561d47f6394a150e9bd74e87
Jan Beneš <jan.benes@iseco.cz>
2019-09-01 01:09:36 +0200

    Fixes of the main functionality

--- automated_configs.py
+++ automated_configs.py
@@ -7,19 +7,18 @@ from jlav import loadAndValidateJSONfile
 
 from time import localtime, strftime
 
-from var_dump import var_dump
+appName = "Gistconfs"
 """
 add some option to have monokai color scheme as well
 https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 there needs to be ~/.vim/colors/<thecolor>.vim
 """
-
 def readFile(filename: str) -> str:
     try:
         with open(filename, 'r') as f:
             return f.read()
     except:
-        printlog("There was error while reading the file.")
+        printlog(appName, "There was error while reading ({}) file.".format(filename))
         quit()
 
 def writeIntoFile(filename: str, content: str) -> str:
@@ -49,26 +48,29 @@ def printlog(appName: str, logText: str, dateTimeFormat = "%d-%m-%Y %H:%M:%S %Z 
 
 
 def main():
-    appName = "Gistconfs"
     try:
         cred = loadAndValidateJSONfile("automated_configs_credentials.json", "automated_configs_credentials.schema")
         r = requests.get("https://api.github.com/users/LordShedy/gists", auth=(cred['username'],cred['token'])).json()
     except:
-        printlog("There was some error while getting URL, network might be down or there might be some issues with the GitHub Gist API.")
+        printlog(appName, "There was some error while getting URL, network might be down or there might be some issues with the GitHub Gist API.")
         quit()
     else:
+        fv = 0
         for i in cred['files']:
+            fv = 1
             gistID = cred['files'][i]['id']
             raw_url = getRawURL(r, i, gistID)
             origin = requests.get(raw_url).text
             path = cred['files'][i]['localPath']
             local = readFile(path)
             if origin != local:
-                var_dump(writeIntoFile(path, origin))
-                return printlog(appName, "There has been a newer version of this config, the file has been updated.")
+                writeIntoFile(path, origin)
+                printlog(appName, "There has been a newer version of ({}) config, the file has been updated.".format(path))
             else:
-                return printlog(appName, "This config file is the same as at the origin, nothing has been changed.")
-        return printlog(appName, "There are no configs to be synchronized, nothing to do.")
+                printlog(appName, "File ({}) is the same as at the origin, nothing has been changed.".format(path))
+        if fv == 0:
+            return printlog(appName, "There are no configs to be synchronized, nothing else to do.")
+        return printlog(appName, "There are no more configs to be synchronized, nothing else to do.")
 
 if __name__ == "__main__":
     main()
