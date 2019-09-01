93ce1e9b82dd5d9b29f5e57f37c7f4a023fde49f
Lord Shedy <lordshedy@pop-os.localdomain>
2019-09-01 17:00:59 +0200

    Improvement of logging

--- automated_configs.py
+++ automated_configs.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests, sys, datetime, time
+import requests, sys, datetime, time, random, zlib
 
 from jlav import loadAndValidateJSONfile
 
 from time import localtime, strftime
 
 appName = "Gistconfs"
-"""
-add some option to have monokai color scheme as well
-https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
-there needs to be ~/.vim/colors/<thecolor>.vim
-"""
+instanceHash = zlib.crc32(bytes(random.randint(1000000,999999999)))
+
 def readFile(filename: str) -> str:
     try:
         with open(filename, 'r') as f:
             return f.read()
     except:
-        printlog(appName, "There was error while reading ({}) file.".format(filename))
+        printlog(appName, instanceHash, "There was error while reading ({}) file.".format(filename))
         quit()
 
 def writeIntoFile(filename: str, content: str) -> str:
@@ -30,29 +27,30 @@ def writeIntoFile(filename: str, content: str) -> str:
             f.truncate()
             return 1
     except:
-        printlog("There was error while reading the file.")
+        printlog(appName, instanceHash, "There was error while reading the file.")
         return 0
 
 def getRawURL(inputObject: dict, fileName: str, gistID: str) -> str:
     for i in inputObject:
       if i['id'] == gistID:
         return i['files'][fileName]['raw_url']
-    return printlog("There was some error while getting URL, possible issue with API.")
+    return printlog(appName, instanceHash, "There was some error while getting URL, possible issue with API.")
 
 
 """
 This function serves for formalized logging
 """
-def printlog(appName: str, logText: str, dateTimeFormat = "%d-%m-%Y %H:%M:%S %Z %z") -> str:
-    return print("[{}]; [{}]; ({}); {}".format(strftime(dateTimeFormat, localtime()),time.time() , appName, logText ))
-
+def printlog(appName: str, instanceHash: str,  logText: str, dateTimeFormat = "%d-%m-%Y %H:%M:%S %Z %z") -> str:
+    dateTime = strftime(dateTimeFormat, localtime())
+    epochTime = time.time()
+    return print("[{} {}]; [{}]; ({}); {}".format(dateTime, epochTime, instanceHash, appName, logText ))
 
 def main():
     try:
         cred = loadAndValidateJSONfile("automated_configs_credentials.json", "automated_configs_credentials.schema")
         r = requests.get("https://api.github.com/users/LordShedy/gists", auth=(cred['username'],cred['token'])).json()
     except:
-        printlog(appName, "There was some error while getting URL, network might be down or there might be some issues with the GitHub Gist API.")
+        printlog(appName, instanceHash, "There was some error while getting URL, network might be down or there might be some issues with the GitHub Gist API.")
         quit()
     else:
         fv = 0
@@ -65,12 +63,12 @@ def main():
             local = readFile(path)
             if origin != local:
                 writeIntoFile(path, origin)
-                printlog(appName, "There has been a newer version of ({}) config, the file has been updated.".format(path))
+                printlog(appName, instanceHash, "There has been a newer version of ({}) config, the file has been updated.".format(path))
             else:
-                printlog(appName, "File ({}) is the same as at the origin, nothing has been changed.".format(path))
+                printlog(appName, instanceHash, "File ({}) is the same as at the origin, nothing has been changed.".format(path))
         if fv == 0:
-            return printlog(appName, "There are no configs to be synchronized, nothing else to do.")
-        return printlog(appName, "There are no more configs to be synchronized, nothing else to do.")
+            return printlog(appName, instanceHash, "There are no configs to be synchronized, nothing else to do.")
+        return printlog(appName, instanceHash, "All configs were synchronized, nothing else to do.")
 
 if __name__ == "__main__":
     main()
